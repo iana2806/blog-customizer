ec8cc87e8753d7ff36821f6a3f9afb460330c428
Iana Rusachenko <iana2806@yandex.ru>
2026-01-29 08:12:43 +0300

    первая итерация

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,123 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import { Select } from 'src/ui/select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	onToggle: () => void;
+	draft: ArticleStateType;
+	onChangeDraft: (value: ArticleStateType) => void;
+	onApply: () => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onToggle,
+	draft,
+	onChangeDraft,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase align='left'>
+						Задайте параметры
+					</Text>
+
+					<div className={styles.spacing}></div>
+
+					<Select
+						title='шрифт'
+						options={fontFamilyOptions}
+						selected={draft.fontFamilyOption}
+						onChange={(option) =>
+							onChangeDraft({ ...draft, fontFamilyOption: option })
+						}
+					/>
+
+					<div className={styles.spacing}></div>
+
+					<RadioGroup
+						name='font-size'
+						title='рАЗМЕР шрифта'
+						options={fontSizeOptions}
+						selected={draft.fontSizeOption}
+						onChange={(option) =>
+							onChangeDraft({ ...draft, fontSizeOption: option })
+						}
+					/>
+
+					<div className={styles.spacing}></div>
+
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={draft.fontColor}
+						onChange={(option) =>
+							onChangeDraft({ ...draft, fontColor: option })
+						}
+					/>
+
+					<div className={styles.spacing}></div>
+
+					<Separator />
+
+					<div className={styles.spacing}></div>
+
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={draft.backgroundColor}
+						onChange={(option) =>
+							onChangeDraft({ ...draft, backgroundColor: option })
+						}
+					/>
+
+					<div className={styles.spacing}></div>
+
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={draft.contentWidth}
+						onChange={(option) =>
+							onChangeDraft({ ...draft, contentWidth: option })
+						}
+					/>
+
+					<div className={styles.spacingLarge}></div>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							type='clear'
+							htmlType='button'
+							onClick={onReset}
+						/>
+						<Button
+							title='Применить'
+							type='apply'
+							htmlType='button'
+							onClick={onApply}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,33 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isParamsOpen, setIsParamsOpen] = useState(false);
+	const [articleSettings, setArticleSettings] = useState(defaultArticleState);
+	const [draftSettings, setDraftSettings] = useState(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleSettings.fontFamilyOption.value,
+					'--font-size': articleSettings.fontSizeOption.value,
+					'--font-color': articleSettings.fontColor.value,
+					'--container-width': articleSettings.contentWidth.value,
+					'--bg-color': articleSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isParamsOpen}
+				onToggle={() => setIsParamsOpen((prev) => !prev)}
+				draft={draftSettings}
+				onChangeDraft={setDraftSettings}
+				onApply={() => setArticleSettings(draftSettings)}
+				onReset={() => {
+					setDraftSettings(defaultArticleState);
+					setArticleSettings(defaultArticleState);
+				}}
+			/>
 			<Article />
 		</main>
 	);
